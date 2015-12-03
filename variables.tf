020bf2a6e584147c9cb90c7b6a7b6048ea397c69
aibou <aibou7251@gmail.com>
2015-12-03 18:24:29 +0900

    init

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  description = "your iam key"
+}
+
+variable "aws_secret_key" {
+  description = "your iam secret key"
+}
+
+variable "aws_region" {
+  description = "ex: ap-northeast-1"
+}
